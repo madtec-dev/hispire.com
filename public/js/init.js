e839ebb69f5769d3c20440ab836f6ed164f425bf
Giorgix <jackosinapsis@gmail.com>
2015-12-08 11:16:12 -0500

    changed img proportion in portfolio

--- public/js/init.js
+++ public/js/init.js
@@ -1,6 +1,6 @@
 $(function() {
   FastClick.attach(document.body);
   $(document).pjax('a[data-pjax]', '#data-container');
-  $(document).foundation();
+  //$(document).foundation();
   new WOW().init();
 })
